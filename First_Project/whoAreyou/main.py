9229c6c20d9efe6ba6a96d60362964d9f49634c1
UCAS-Averyyyyyy <avery.bowman@ucas-edu.net>
2024-09-11 09:25:13 -0600

    Age, name, color

---
+++ First_Project/whoAreyou/main.py
@@ -0,0 +1,9 @@
+#Avery, who are you?
+
+name=input("enter your name")
+
+age = float(input("enter your age: "))
+
+color = input("Whats your favorite color?: ")
+
+print("Hello,",name,"welcome! You look good for your age,",age, (color)":")
