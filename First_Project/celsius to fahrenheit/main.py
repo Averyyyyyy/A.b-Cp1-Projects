214ac9002e9bceaaf3c2d3c83d4465efc962753d
UCAS-Averyyyyyy <avery.bowman@ucas-edu.net>
2024-09-20 09:52:46 -0600

    started to make the equation for celsuis

---
+++ First_Project/celsius to fahrenheit/main.py
@@ -0,0 +1,7 @@
+#hello world
+
+celsius = input("whats is the temperature in celsius for today")
+
+fahrenheit = (fahrenheit == celsius *9/5)+32
+
+print(f"When it is ",celsius," degrees celsius it is ",fahrenheit," degrees fahrenheit")
