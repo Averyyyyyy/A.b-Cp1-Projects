91f226280d13308823ca9255c392966f3658333c
UCAS-Averyyyyyy <avery.bowman@ucas-edu.net>
2024-09-25 09:45:52 -0600

    Fixed th str

--- First_Project/celsius to fahrenheit/main.py
+++ First_Project/celsius to fahrenheit/main.py
@@ -2,6 +2,6 @@
 
 celsius = input("whats is the temperature in celsius for today")
 
-fahrenheit = (fahrenheit == celsius *9/5)+32
+fahrenheit = (float(celsius) *9/5)+32
 
-print(f"When it is ",celsius," degrees celsius it is ",fahrenheit," degrees fahrenheit")
+print(f"When it is "+celsius+" degrees celsius it is "+str(fahrenheit)+" degrees fahrenheit")
