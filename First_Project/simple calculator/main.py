ed5c0ee70e3eb3d3cd5be2f7c874aa0c96d49535
UCAS-Averyyyyyy <avery.bowman@ucas-edu.net>
2024-09-27 09:26:18 -0600

    added simple calculator

---
+++ First_Project/simple calculator/main.py
@@ -0,0 +1,19 @@
+#avery simple calculator
+
+num1=int(input("what do you want the first question to be?; "))
+
+num2=int(input("what do you want the second question to be?; "))
+
+print("addition: ", num1+num2)
+
+print("subtraction: ", num1-num2)
+
+print("multiple: ", num1*num2)
+
+print("divide: ", num1/num2)
+
+print("modulase: ", num1%num2)
+
+print("floor division: ", num1//num2)
+
+print("exponent: ", num1**num2)
