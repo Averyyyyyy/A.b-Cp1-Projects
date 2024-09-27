b79841f609b958cc097b7f481a138c9faafa1f20
UCAS-Averyyyyyy <avery.bowman@ucas-edu.net>
2024-09-27 09:32:54 -0600

    average grade

---
+++ First_Project/average grade/main.py
@@ -0,0 +1,21 @@
+#avery average grade
+
+class1=int(input("what is your grade for your first class?: "))
+
+class2=int(input("what is your grade for your second class?: "))
+
+class3=int(input("what is your grade for your third class?: "))
+
+class4=int(input("what is your grade for your forth class?: "))
+
+class5=int(input("what is your grade for your fifth class?: "))
+
+class6=int(input("what is your grade for your sixth class?: "))
+
+class7=int(input("what is your grade for your seventh class?: "))
+
+class8=int(input("what is your grade for your eighth class?: "))
+
+output=(class1+class2+class3+class4+class5+class6+class7+class8)/8
+
+print("your average grade is.", output)
