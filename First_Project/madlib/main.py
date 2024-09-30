10b804b4d746300b88a31993f6eda8bb2ca7363c
UCAS-Averyyyyyy <avery.bowman@ucas-edu.net>
2024-09-30 08:54:34 -0600

    made madlib

---
+++ First_Project/madlib/main.py
@@ -0,0 +1,15 @@
+#madlib avery
+
+
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+adjective1 = input("Enter an adjective: ")
+noun2 = input("Enter another noun: ")
+place = input("Enter a place: ")
+
+
+madlib = f"One day, a {adjective1} {noun1} decided to {verb1} near the {noun2} in {place}. It was a very strange sight!"
+
+
+print("\nYour Madlib story is:\n")
+print(madlib)
