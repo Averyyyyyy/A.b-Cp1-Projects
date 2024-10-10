fb3af3b9520aa099f1dd72530408231432afa412
UCAS-Averyyyyyy <avery.bowman@ucas-edu.net>
2024-10-10 09:12:14 -0600

    made shopping list

---
+++ First_Project/madlib/list of classes/main.py
@@ -0,0 +1,11 @@
+#avery lists of classes
+
+
+classes = ["Computer Science", "Leadership", "Math", "Advisory", "Chemistry", "English", "Art"]
+
+
+print("List of classes:", classes)
+
+
+for class_name in classes:
+   print("Class:", class_name)
---
+++ First_Project/madlib/palindrome/main.py
@@ -0,0 +1,15 @@
+#avery palindrome
+
+
+Def: is_palindrome(word);
+Cleaned_world = word.lower().replace(“ “, “”):
+
+
+If cleaned_word == cleaned_word[::-1]:
+print (f’“{word}” is a palindrome.‘ ):
+else:
+print (f’“{word}” is not a palindrome.‘ ):
+
+
+User_input = input(“Enter a word or phrase: “):
+is_palindrome(user_input):
