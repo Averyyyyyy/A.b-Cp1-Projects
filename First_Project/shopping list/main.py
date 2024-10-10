a16b5d9d4d94e4d19018f500ab34663aa4603a50
UCAS-Averyyyyyy <avery.bowman@ucas-edu.net>
2024-10-10 09:19:38 -0600

    actually made my shopping list this time, the other one was of the classes

--- First_Project/shopping list/main.py
+++ First_Project/shopping list/main.py
@@ -1,11 +1,38 @@
-#avery lists of classes
+#avery shopping list
 
 
-classes = ["Computer Science", "Leadership", "Math", "Advisory", "Chemistry", "English", "Art"]
+shopping_list = []
 
 
-print("List of classes:", classes)
+def add():
+   item = input("Enter an item to add: ")
+   shopping_list.append(item)
+   print(f'Added "{item}" to the shopping list.')
 
 
-for class_name in classes:
-   print("Class:", class_name)
+   def remove():
+       item = input("Enter an item to remove: ")
+       if item in shopping_list:
+           shopping_list.remove(item)
+           print(f'"{item}" is not in the shopping list.')
+
+
+           while True:
+               action = input("""
+                              What would you like to do?
+                              Enter 1 to add an item
+                              Enter 2 to remove an item
+                              Enter 3 to leave the list
+                              """)
+               if action == "1":
+                   add()
+               elif action == "2":
+                   remove()
+               elif action == "3":
+                   print("Have a nice day!")
+                   break
+               else:
+                   print("Invalid input, please try again.")
+
+
+                   print("\nCurrent Shopping List:", shopping_list)
