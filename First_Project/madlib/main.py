124829b68872b80f4df419cdc9b9936075923b05
UCAS-Averyyyyyy <avery.bowman@ucas-edu.net>
2024-10-21 09:12:04 -0600

    fixed madlib

--- First_Project/madlib/main.py
+++ First_Project/madlib/main.py
@@ -1,15 +1,20 @@
-#madlib avery
+#Madlib game
+#Author: Avery
 
 
-noun1 = input("Enter a noun: ")
-verb1 = input("Enter a verb: ")
-adjective1 = input("Enter an adjective: ")
-noun2 = input("Enter another noun: ")
-place = input("Enter a place: ")
+#user inputs
+place = input("Enter a place: ")   #Example "paris"
+adjective = input("Enter an adjective: ")   #example "funny"
+noun = input("Enter a noun: ")   #example "dog"
+verb = input("Enter a verb: ")   #example "run"
+plural_noun = input("Enter a plural noun: ")   #example "cars"
+celebrity = input("enter the name of a celebrity: ")   #example "tom holand"
 
 
-madlib = f"One day, a {adjective1} {noun1} decided to {verb1} near the {noun2} in {place}. It was a very strange sight!"
+#madlib story using the inputs
+madlib = f"One day, I went to {place} and saw a very {adjective} {noun}. It started to {verb} around the {place} and everyone was watching. Suddenly a group of {plural_noun} appeared and joined the fun! Even {celebrity} coudn't belive it!"
 
 
-print("\nYour Madlib story is:\n")
+#print the mablib story
+print("\nHere is your Maddlib:\n")
 print(madlib)
