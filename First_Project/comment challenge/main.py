d236d4dd691eba2df0aedde4b8406f5a4217fb00
UCAS-Averyyyyyy <avery.bowman@ucas-edu.net>
2024-10-21 08:54:31 -0600

    added comment challenge

---
+++ First_Project/comment challenge/main.py
@@ -0,0 +1,13 @@
+#This program asks the user for their name and displays a greeting
+#Author: Avery
+
+
+#Prompt the user to enter their name
+user_name = input("Please enter your name: ")  #Input function stores user input in user_name variable
+
+
+#Greet the user by name
+print("Hello, " + user_name + "!") #Displays a greeting message with the user's name
+
+
+#End of the program	
