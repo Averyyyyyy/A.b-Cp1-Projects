d8f0743fa54d8e25062e09b2f79ea5ef69b477be
Averyyyyyy <Avery.bowman@ucas-edu.net>
2024-11-08 09:51:54 -0700

    added personal infomation

---
+++ Personal infomation/main.py
@@ -0,0 +1,20 @@
+#avery bowman, personal infomation
+
+name = input("What is your name?")
+age = input("What is your age?")
+height = input("what is you height in meters?")
+favorite_number = input("What is your favorite number?")
+
+print("Your name is"+ name)
+print("Your age"+ age)
+print("Your height"+ height)
+print("Your favorite number"+ favorite_number)
+
+age = int(age)
+favorite_number = int(favorite_number)
+height = float(height)
+
+print("Your name is"+ str(name))
+print("Your age"+ str(age))
+print("Your height"+ str(height))
+print("Your favorite_number"+ str(favorite_number))
